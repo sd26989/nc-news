a394e425bde6236092c95f270c08d021a7703f97
Scott Dyer <scottdyer@Scotts-MacBook-Air.local>
2023-05-15 15:45:57 +0100

    implemented sorting of articles

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,30 +1,89 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Articles = ({ articles, loading }) => {
-    
-    if (loading) {
-        return <p>Loading...</p>
+  const [sortBy, setSortBy] = useState("date");
+  const [sortOrder, setSortOrder] = useState("desc");
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const sortedArticles = articles.sort((a, b) => {
+    let result = 0;
+    if (sortBy === "date") {
+      const dateA = new Date(a.created_at);
+      const dateB = new Date(b.created_at);
+      result = dateB - dateA;
+    } else if (sortBy === "votes") {
+      result = b.votes - a.votes;
+    } else if (sortBy === "comments") {
+        result = b.comment_count - a.comment_count;
+      }
+
+    if (sortOrder === "asc") {
+      result *= -1;
     }
+    return result;
+  });
 
-    return (
-        <main>
-            <h2>All Articles</h2>
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+  };
 
-            <ul className='allArticles'>
-           {articles.map((article) => {
-            return <li className='articles' key={article.article_id}>
-                <Link to={`/articles/${article.article_id}`}><h2>{article.title}</h2></Link>
-                <img src={article.article_img_url} alt={`${article.topic}`} className={'responsiveImg'}></img>
-                <p>Written by {article.author}</p>
-                <p>Topic: {article.topic}</p>
-                <p>Votes: {article.votes}</p>
-                <p>Comments:{article.comment_count}</p>
-                </li>
-           })}
-        </ul>
+  const handleOrderChange = (e) => {
+    setSortOrder(e.target.value);
+  };
 
-        </main>
-    );
+  return (
+    <main>
+      <h2>All Articles</h2>
+
+      <div>
+        <label htmlFor="sort">Sort By:</label>
+        <select id="sort" value={sortBy} onChange={handleSortChange}>
+          <option value="date">Date</option>
+          <option value="votes">Votes</option>
+          <option value="comments">Comments</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="order">Sort Order:</label>
+        <select id="order" value={sortOrder} onChange={handleOrderChange}>
+          <option value="desc">Descending</option>
+          <option value="asc">Ascending</option>
+        </select>
+      </div>
+
+      <ul className="allArticles">
+        {sortedArticles.map((article) => {
+          return (
+            <li className="articles" key={article.article_id}>
+              <Link to={`/articles/${article.article_id}`}>
+                <h2>{article.title}</h2>
+              </Link>
+              <img
+                src={article.article_img_url}
+                alt={`${article.topic}`}
+                className={"responsiveImg"}
+              ></img>
+              <p>Written by {article.author}</p>
+              <p>Topic: {article.topic}</p>
+              <p>Votes: {article.votes}</p>
+              <p>Comments: {article.comment_count}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 };
 
 export default Articles;
+
+
+
+
+
+
